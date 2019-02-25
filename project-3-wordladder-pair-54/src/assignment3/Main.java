a6cc82b6a1047a1bfac3126073d028c34d7204e3
Nikhil Arora <narora205@gmail.com>
2019-02-25 00:02:05 -0600

    Test

--- project-3-wordladder-pair-54/src/assignment3/Main.java
+++ project-3-wordladder-pair-54/src/assignment3/Main.java
@@ -1,14 +1,14 @@
 /* WORD LADDER Main.java
  * EE422C Project 3 submission by
  * Replace <...> with your actual data.
- * <Nikhil Arora>
- * <na24464>
- * <16215>
+ * <Student1 Name>
+ * <Student1 EID>
+ * <Student1 5-digit Unique No.>
  * <Student2 Name>
  * <Student2 EID>
  * <Student2 5-digit Unique No.>
  * Slip days used: <0>
- * Git URL: https://github.com/EE422C/project-3-wordladder-pair-54
+ * Git URL:
  * Spring 2019
  */
 
